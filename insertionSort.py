db503cae3f133465224e4076a853e77c2ffd292d
tkimweston <50267585+tkimweston@users.noreply.github.com>
2021-02-17 17:49:01 -0500

    Add files via upload

--- insertionSort.py
+++ insertionSort.py
@@ -1,30 +1,15 @@
 def insertionSort(array):
     for i in range(1, len(array)):
         key = array[i]
-        j = i -1
-        while j >= 0 and key < array[j]:
-            print(i, j)
+        j = i - 1
+        while j >= 0 and array[j] > key:
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = key
-    return array
-    
-a = [10, 5, 3, 8, 2, 9, 7, 1, 6, 4]
-print(insertionSort(a))
 
-"""
-5 10 3 8 2 9 7 1 6 4
-5 3 10 8 2 9 7 1 6 4
-3 5 10 8 2 9 7 1 6 4
-3 5 8 10 2 9 7 1 6 4
-3 5 8 2 10 9 7 1 6 4
-3 5 2 8 10 9 7 1 6 4
-3 2 5 8 10 9 7 1 6 4
-2 3 5 8 10 9 7 1 6 4
-2 3 5 8 9 10 7 1 6 4
-2 3 5 8 9 7 10 1 6 4
-2 3 5 8 7 9 10 1 6 4
-2 3 5 7 8 9 10 1 6 4
-2 3 5 7 8 ...
-1 2 3 4 5 6 7 8 9 10
-"""
+a = [5, 3, 6, 7, 8, 2, 10]
+insertionSort(a)
+print(a)
+a = [31, 41, 59, 26, 41, 58]
+insertionSort(a)
+print(a)
